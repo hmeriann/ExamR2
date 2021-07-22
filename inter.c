98cf00cf6a1975b759f9d984fe34ebe191c7dcaf
Zuleykha Pavlichenkova <ya-zu@yandex.ru>
2021-07-22 13:49:15 +0300

    fixed inter

--- inter.c
+++ inter.c
@@ -1,49 +1,24 @@
-/*
-Assignment name  : inter
-Expected files   : inter.c
-Allowed functions: write
---------------------------------------------------------------------------------
-en
-Write a program that takes two strings and displays, without doubles, the
-characters that appear in both strings, in the order they appear in the first one.
 
-The display will be followed by a \n.
-
-If the number of arguments is not 2, the program displays \n.
-----------------------------------------------------------------------------------
-
-Examples:
-
-$>./inter "padinton" "paqefwtdjetyiytjneytjoeyjnejeyj" | cat -e
-padinto$
-$>./inter ddf6vewg64f gtwthgdwthdwfteewhrtag6h4ffdhsd | cat -e
-df6ewg4$
-$>./inter "nothing" "This sentence hides nothing" | cat -e
-nothig$
-$>./inter | cat -e
-$
-
-*/
 #include <unistd.h>
 
 int main(int argc, char **argv)
 {
-	int i;
+	int i = 0;
 	int j;
-	int flags[128] = {0};
-	
-	i = 0;
-	if (argc == 3) //это условие необходимо для того, чтобы обрабатывать только случаи с двумя аргументами. Первый аргумент - это название функции, поэтому проверяем, что аргументов 3
+	int flags[255] = {0};
+
+	if (argc == 3)
 	{
 		while (argv[1][i] != '\0')
 		{
 			j = 0;
-			if (argv[1][i] == argv[1][i + j])
+			while (argv[2][i + j] != '\0')
 			{
-				flags[argv[1][i]] = 1;
+				if (argv[1][i] == argv[2][i + j]) 
+					flags[argv[1][i]] = 1;
 				j++;
 			}
-		i++;
+			i++;
 		}
 		i = 0;
 		while (argv[1][i] != '\0')
